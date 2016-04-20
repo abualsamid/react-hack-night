4e07d8fd0433fb5f221495b5618f2b3cf57657ca
Abualsamid <ahmad@abualsamid.com>
2016-04-19 21:40:45 -0400

    last commit

--- logic/index.js
+++ logic/index.js
@@ -7,11 +7,20 @@ export default function processMessage(value) {
 
   if (value.startsWith("/learn")) {
     var msg = value.substring(6,value.indexOf("/reply")).trim()
-    var learned = value.substring(value.indexOf("/reply")+6)
-    console.log("took " , msg, " learned ", learned)
+    var learned = value.substring(value.indexOf("/reply")+6).trim()
     whatIKnow[msg] = learned;
     return 'I learned to reply to "' + msg + '" with: "' + learned + '""';
   }
+
+  whatIKnow.forEach(function(item, key) {
+    var pat = new RegExp(key)
+    console.log("testing ", key, "against ", pat, " for ", value)
+    if (pat.test(value)) {
+      return p[key]
+    }
+
+  });
+
   if (whatIKnow[value]) {
     return whatIKnow[value]
   }
